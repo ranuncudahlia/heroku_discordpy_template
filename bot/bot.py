ff82d84ddd81d4e2a0ac9dcfb08ef3b6f09644e2
ranuncudahlia <ranuncudahlia@gmail.com>
2021-01-28 15:22:26 -0600

    Added the coding needed for syncing with Heroku

--- bot/bot.py
+++ bot/bot.py
@@ -4,3 +4,6 @@
 # For Heroku, make sure your .py includes the following.
 import os # import the OS details, including our hidden bot token
 token = os.environ.get('DISCORD_BOT_TOKEN') # fetch the token from Heroku's "OS" running the bot. make sure the name matches the one you've used on Heroku
+
+# Include this at the end of your code. Instead of bot, you may have "discord.Client()" "commands.Bot()" etc, or whatever you have defined these.
+bot.run(token) # make sure your token variable matches the token defined above
