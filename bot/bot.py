88a2fca936a584f95f2722badee5f7749ce0768d
ranuncudahlia <ranuncudahlia@gmail.com>
2021-01-28 15:19:29 -0600

    Added the coding needed for syncing with Heroku

--- bot/bot.py
+++ bot/bot.py
@@ -1,1 +1,6 @@
-# Bot file goes here.
+### Bot file goes here. You can either upload your own or simply replace this file.
+### If you upload your own file or rename the .py, make sure to change the directory location in our Procfile.
+
+# For Heroku, make sure your .py includes the following.
+import os # import the OS details, including our hidden bot token
+token = os.environ.get('DISCORD_BOT_TOKEN') # fetch the token from Heroku's "OS" running the bot. make sure the name matches the one you've used on Heroku
